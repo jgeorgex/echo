39a2402369842c79cb9e4cbe1a31ebee082b38a4
Julian George <jules104@gmail.com>
2020-01-13 15:01:25 +0000

    Add get_user_input method

--- lib/echo.rb
+++ lib/echo.rb
@@ -4,5 +4,8 @@ def run_app
   "2018-01-09 | 16:26 | You said: 'hello, world'!"
 end
 
+def get_user_input
+  'hello world'
+end
 
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -6,3 +6,10 @@ require 'echo'
       expect(echo.run_app).to eq("2018-01-09 | 16:26 | You said: 'hello, world'!")
     end
   end
+
+  describe '#get_user_input' do
+    it 'returns hello world! when input is hello world' do
+    echo = Echo.new
+    expect(echo.get_user_input).to eq('hello world')
+    end
+  end 
